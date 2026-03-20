ae86dee43f7cecdcd9262c9383ed73626a52e422
Gui Vecchi <vecchigm@gmail.com>
2026-03-19 19:47:03 -0600

    move to neon for postgres

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient() {
-  const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+  const adapter = new PrismaNeon({
+    connectionString: process.env.DATABASE_URL!,
   });
   return new PrismaClient({ adapter });
 }
